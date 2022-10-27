7dcff2594b93af9be0f6885da3d89f26aaf1ac8a
rudyjm3 <rudyjm3@yahoo.com>
2022-10-26 21:44:19 -0400

    Added event listener to links in profile card. Working on adding active link class depending on which was clicked.

--- js/app.js
+++ js/app.js
@@ -8,7 +8,49 @@ fetch('data.json')
    })
    .then(function(data) {
       console.log(data);
-      console.log(data[0]);
    });
+// Once json data is returned, then run named function
+fetch('data.json')
+   .then((data) => {
+      return data.json();
+   }).then(myFunction);
 
+// json data passed into functions argument
+function myFunction(data) {
+   console.log(data);
+   const workData = data[0];
+   console.log(workData.timeframes.daily.current);
+}
 
+// Get all link elements
+let timeFrame = document.querySelectorAll('.time-frame');
+
+// For each link element
+timeFrame.forEach((ele) => {
+   // Listen for which was clicked
+   ele.addEventListener('click', (evt) => {
+      // If the one that was clicked text = condition, fire alert
+      if (ele.innerText == 'Work') {
+         alert("Work link clicked");
+         // If element text did not match condition above, fire alert below
+      } else {
+         alert(ele.innerText + " Was clicked.")
+      }
+   })
+});
+
+timeFrame.forEach((ele) => {
+   // ele.classList.remove('active-tf')
+   // console.log("I ran");
+   
+   // Listen for which was clicked
+   ele.addEventListener('click', (evt) => {
+      // If the one that was clicked text = condition, fire alert
+      if (ele.classList.contains('active-tf')) {
+         alert("Work link clicked");
+         // If element text did not match condition above, fire alert below
+      } else {
+         alert(ele.innerText + " Was clicked.")
+      }
+   })
+});
